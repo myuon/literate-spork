f3d0aeeeebe9cb981509d794f0414a4a59f64633
myuon <ioi.joi.koi.loi@gmail.com>
2022-11-14 03:16:09 +0900

    feat: show image thumbnails

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -88,25 +88,56 @@ const SelectFiles = ({ onChange }: { onChange: (files: File[]) => void }) => {
   );
 };
 
-const styles = {
-  resetButton: css`
-    padding: 0;
-    font: inherit;
-    color: inherit;
-    cursor: pointer;
-    background: none;
-    border: none;
-  `,
-};
-
 function App() {
+  const [images, setImages] = useState<string[]>([]);
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      css={css`
+        display: grid;
+        gap: 32px;
+      `}
+    >
       <SelectFiles
         onChange={(files) => {
+          const reader = new FileReader();
+
+          files.forEach((file) => {
+            reader.addEventListener("load", (event) => {
+              const result = event.target?.result;
+              if (result) {
+                setImages((images) => [...images, result as string]);
+              }
+            });
+            reader.readAsDataURL(file);
+          });
           console.log(files);
         }}
       />
+
+      <div
+        css={css`
+          display: grid;
+          grid-template-columns: repeat(4, 1fr);
+          gap: 24px;
+        `}
+      >
+        {images.map((image, index) => (
+          <div key={index}>
+            <img
+              src={image}
+              alt={`${index}`}
+              css={css`
+                width: 100%;
+                aspect-ratio: 1;
+                border-radius: 8px;
+                object-fit: cover;
+              `}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
