f9407807c00894928ff72aae31a0415c84a66c91
myuon <ioi.joi.koi.loi@gmail.com>
2022-11-14 02:45:40 +0900

    feat: DropArea

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,52 +1,102 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { css } from "@emotion/react";
+import { useRef, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [resp, setResp] = useState("");
-  useEffect(() => {
-    void (async () => {
-      const resp = await fetch("/api/hello");
-      if (resp.ok) {
-        setResp(await resp.text());
-      }
-    })();
-  }, []);
+const DropArea = ({ onChange }: { onChange: () => void }) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{resp ?? "Vite + React"}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <div
+    <>
+      <input
+        type="file"
+        multiple
+        accept="image/*"
         css={css`
-          padding: 10px;
-          background-color: #f9f9f9;
-          border: 1px solid #ddd;
+          display: none;
         `}
+        ref={inputRef}
+        onChange={() => {
+          onChange();
+        }}
+      />
+      <div
+        role="button"
+        tabIndex={0}
+        data-dragging={isDragging}
+        css={[
+          css`
+            display: grid;
+            place-items: center;
+            min-height: 160px;
+            color: rgba(0, 0, 0, 0.6);
+            cursor: pointer;
+            border: 2px dashed rgba(0, 0, 0, 0.2);
+            border-radius: 8px;
+          `,
+          css`
+            &[data-dragging="true"] {
+              border-color: rgba(0, 0, 0, 0.6);
+            }
+          `,
+        ]}
+        onClick={() => {
+          inputRef.current?.click();
+        }}
+        onKeyDown={() => {
+          inputRef.current?.click();
+        }}
+        onDragOver={(event) => {
+          event.preventDefault();
+          setIsDragging(true);
+        }}
+        onDragLeave={(event) => {
+          event.preventDefault();
+          setIsDragging(false);
+        }}
+        onDrop={(event) => {
+          event.preventDefault();
+          setIsDragging(false);
+          onChange();
+        }}
       >
-        EMOTION INSTALLED
+        Drop files here OR click to select files
       </div>
+    </>
+  );
+};
+
+const styles = {
+  resetButton: css`
+    padding: 0;
+    font: inherit;
+    color: inherit;
+    cursor: pointer;
+    background: none;
+    border: none;
+  `,
+};
+
+function App() {
+  return (
+    <div className="App">
+      <DropArea
+        onChange={() => {
+          console.log("change");
+        }}
+      />
+      <button
+        css={[
+          styles.resetButton,
+          css`
+            padding: 8px 16px;
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            border-radius: 8px;
+          `,
+        ]}
+      >
+        SELECT FILES
+      </button>
     </div>
   );
 }
