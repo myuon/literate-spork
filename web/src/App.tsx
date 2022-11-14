9e4234afc58754982cd77fed2f69ef494ba927e7
myuon <ioi.joi.koi.loi@gmail.com>
2022-11-15 01:54:02 +0900

    feat: upload with semaphore

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,60 @@
 import "./App.css";
 import { css } from "@emotion/react";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { SelectFiles } from "./components/SelectFiles";
 import { Thumbnail } from "./components/Thumbnail";
 import { Button } from "./components/Button";
 
+const sleep = async (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const upload = async (file: File) => {
+  return sleep(Math.random() * 8000);
+};
+
+const SEMAPHORE_SIZE = 3;
+
 function App() {
   const [images, setImages] = useState<File[]>([]);
+
+  const [uploadTaskQueue, setUploadTaskQueue] = useState<number[]>([]);
+  const isUploading = uploadTaskQueue.length > 0;
+  const [semaphore, setSemaphore] = useState(0);
+
+  const startUpload = async (tasks: number[]) => {
+    setUploadTaskQueue(tasks);
+  };
+  const runQueue = useCallback(async () => {
+    if (uploadTaskQueue.length === 0) {
+      return;
+    }
+    if (semaphore >= SEMAPHORE_SIZE) {
+      return;
+    }
+
+    const [task, ...rest] = uploadTaskQueue;
+    setUploadTaskQueue(rest);
+    setSemaphore((p) => p + 1);
+    setImages((images) => {
+      const newImages = [...images];
+      newImages[task].status = "uploading";
+      return newImages;
+    });
+    await upload(images[task]);
+    setImages((images) => {
+      const newImages = [...images];
+      newImages[task].status = "completed";
+      return newImages;
+    });
+    setSemaphore((p) => p - 1);
+  }, [images, semaphore, uploadTaskQueue]);
+
+  useEffect(() => {
+    if (isUploading) {
+      runQueue();
+    }
+  }, [isUploading, runQueue]);
 
   return (
     <div
@@ -31,7 +79,12 @@ function App() {
       >
         {images.map((image, index) => (
           <div key={index}>
-            <Thumbnail blob={image} alt={`${index}`} />
+            <Thumbnail
+              blob={image}
+              alt={`${index}`}
+              uploading={image.status === "uploading"}
+              completed={image.status === "completed"}
+            />
           </div>
         ))}
       </div>
@@ -42,7 +95,14 @@ function App() {
           justify-content: flex-end;
         `}
       >
-        <Button>Submit</Button>
+        <Button
+          disabled={isUploading}
+          onClick={async () => {
+            startUpload(images.map((_, index) => index));
+          }}
+        >
+          Upload
+        </Button>
       </div>
     </div>
   );
--- web/src/components/Thumbnail.tsx
+++ web/src/components/Thumbnail.tsx
@@ -1,22 +1,74 @@
 import { css } from "@emotion/react";
 
-export const Thumbnail = ({ blob, alt }: { blob: Blob; alt: string }) => {
+export const Thumbnail = ({
+  blob,
+  alt,
+  uploading,
+  completed,
+}: {
+  blob: Blob;
+  alt: string;
+  uploading?: boolean;
+  completed?: boolean;
+}) => {
   const src = URL.createObjectURL(blob);
 
   return (
-    <img
-      src={src}
-      alt={alt}
+    <div
       css={css`
-        width: 100%;
-        aspect-ratio: 1;
-        background-color: #eee;
-        border-radius: 8px;
-        object-fit: cover;
+        position: relative;
       `}
-      onLoad={() => {
-        URL.revokeObjectURL(src);
-      }}
-    />
+    >
+      <img
+        src={src}
+        alt={alt}
+        css={css`
+          width: 100%;
+          aspect-ratio: 1;
+          background-color: #eee;
+          border-radius: 8px;
+          object-fit: cover;
+        `}
+        onLoad={() => {
+          URL.revokeObjectURL(src);
+        }}
+      />
+      {uploading ? (
+        <div
+          css={css`
+            position: absolute;
+            top: 0;
+            left: 0;
+            display: grid;
+            place-items: center;
+            width: 100%;
+            height: 100%;
+            color: white;
+            background-color: rgba(0, 0, 0, 0.5);
+            border-radius: 8px;
+          `}
+        >
+          UPLOADING...
+        </div>
+      ) : null}
+      {completed ? (
+        <div
+          css={css`
+            position: absolute;
+            top: 0;
+            left: 0;
+            display: grid;
+            place-items: center;
+            width: 100%;
+            height: 100%;
+            color: white;
+            background-color: rgba(0, 0, 0, 0.5);
+            border-radius: 8px;
+          `}
+        >
+          ✅
+        </div>
+      ) : null}
+    </div>
   );
 };
